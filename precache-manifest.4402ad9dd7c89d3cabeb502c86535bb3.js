44282fadc892f51436d1e3a35affcd8245fb0028
vandlaw7 <vandlaw@naver.com>
2020-03-03 01:12:59 +0900

    Updates

---
+++ precache-manifest.4402ad9dd7c89d3cabeb502c86535bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc28daded5ec9c648aa8f91ebd425cb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "6f455e77cae20f9e4aeb",
+    "url": "/movie_app_2019/static/css/main.7c148d43.chunk.css"
+  },
+  {
+    "revision": "475468186b9d129af88a",
+    "url": "/movie_app_2019/static/js/2.137c9ea0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2019/static/js/2.137c9ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f455e77cae20f9e4aeb",
+    "url": "/movie_app_2019/static/js/main.f69c14ab.chunk.js"
+  },
+  {
+    "revision": "0f87585caaeceef6a69a",
+    "url": "/movie_app_2019/static/js/runtime-main.da932103.js"
+  }
+]);
